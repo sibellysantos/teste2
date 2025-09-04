de047cb5ab0854fe2d0f46e0cd092a6965b02d2c
sibellysantos <sibelly.cruz.santos@escola.pr.gov.br>
2025-09-04 09:05:15 -0300

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+const avanca = document. queryselectorA11('.btn-proximo');
+const reiniciarBtn = document.getE1ementById('.btn-reniciar');
+
+avanca.forEach(button => {
+button.addEventListener('click',function(){
+    const atual = document.querySelector('.ativo');
+    const proximopasso='passo-'+this.getAttribute('data-proximo')
+})
+
+
+} 
+
+
+
+)
