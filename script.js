f0bfd62cb60c69098192642e55188cee01d8356f
sibellysantos <sibelly.cruz.santos@escola.pr.gov.br>
2025-09-11 08:45:23 -0300

    ok

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ avanca.forEach(button => {
 button.addEventListener('click',function(){
     const atual = document.querySelector('.ativo');
     const proximopasso='passo-'+this.getAttribute('data-proximo')
+atual.clslist.remove('ativo');
+const proximoElemento= document.gtElemenbyId(proximoPasso);
+
+
+
+if (proxmoElemento){
+    proximoElemento.classList.add('ativo');
+} else {
+    console.error(`Elemento com ID"${proximoPasso}não encontrado.`)
+}
+
+
+
+
+
 })
-
-
 } 
 
 
